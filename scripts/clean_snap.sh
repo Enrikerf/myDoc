bb6078c818d26fc8cc59462187ee69619ea37770
EnriqueAA <enrike9066@gmail.com>
2021-11-23 09:20:29 +0100

    doc

--- scripts/clean_snap.sh
+++ scripts/clean_snap.sh
@@ -1,6 +1,8 @@
 !/bin/bash
  #Removes old revisions of snaps
  #CLOSE ALL SNAPS BEFORE RUNNING THIS
+ # https://www.linuxuprising.com/2019/04/how-to-remove-old-snap-versions-to-free.html
+ # https://unix.stackexchange.com/questions/590319/is-it-safe-to-delete-these-snaps
  set -eu
  LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
      while read snapname revision; do
